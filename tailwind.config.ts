28678e8b5f889fb5668dd66aa0f1a1b4e1820bdc
Thomas CESBRON <thomas.cesbron@avec.fr>
2024-08-16 16:07:05 +0200

    feat(tailwind): config tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,22 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#4F46E5", // Exemples de couleurs personnalisées
+        secondary: "#EC4899",
+        foreground: 'rgb(var(--foreground-rgb))',
+        backgroundStart: 'rgb(var(--background-start-rgb))',
+        backgroundEnd: 'rgb(var(--background-end-rgb))',
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], // Ajout d'une police personnalisée
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'), // Plugin pour styliser les formulaires
+    require('@tailwindcss/typography'), // Plugin pour la typographie
+  ],
 };
+
 export default config;
