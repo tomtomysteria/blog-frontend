b33cbfb8097da955c4c8504e97450d00752be77e
Thomas CESBRON <thomas.cesbron@avec.fr>
2024-08-29 18:36:21 +0200

    chore(deps): add @types/json5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        primary: "#4F46E5", // Exemples de couleurs personnalisées
-        secondary: "#EC4899",
+        primary: '#4F46E5', // Exemples de couleurs personnalisées
+        secondary: '#EC4899',
         foreground: 'rgb(var(--foreground-rgb))',
         backgroundStart: 'rgb(var(--background-start-rgb))',
         backgroundEnd: 'rgb(var(--background-end-rgb))',
       },
       fontFamily: {
-        sans: ["Inter", "sans-serif"], // Ajout d'une police personnalisée
+        sans: ['Inter', 'sans-serif'], // Ajout d'une police personnalisée
       },
     },
   },
