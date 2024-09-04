459f357f18ca5bd58736f848df39c36cfdf1e0cd
Thomas CESBRON <thomas.cesbron@avec.fr>
2024-09-04 14:54:26 +0200

    fix(app): detect client side to resolve hydratation problems

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,16 @@
 
 import Link from 'next/link';
 import { useAuth } from '@/context/AuthContext';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
   const { accessToken, logout } = useAuth();
+  const [isClient, setIsClient] = useState(false);
+
+  // Vérification que nous sommes côté client après l'hydratation
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <nav className="flex items-center justify-between p-4 bg-gray-800 text-white mb-4">
@@ -14,12 +21,17 @@ const Navbar = () => {
         </Link>
       </div>
       <div>
-        {accessToken ? (
-          <button onClick={logout}>Logout</button>
+        {isClient ? (
+          accessToken ? (
+            <button onClick={logout}>Logout</button>
+          ) : (
+            <Link href="/login">
+              <span className="text-lg">Login</span>
+            </Link>
+          )
         ) : (
-          <Link href="/login">
-            <span className="text-lg">Login</span>
-          </Link>
+          // Affiche un état de chargement avant que l'hydratation soit terminée
+          <span>Loading...</span>
         )}
       </div>
     </nav>
